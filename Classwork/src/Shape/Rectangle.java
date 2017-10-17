d20e9e6ecb00e575a527e8d6c21cec68e72bbb5a
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2017-10-17 13:14:03 -0400

    completed these classes

--- Classwork/src/Shape/Rectangle.java
+++ Classwork/src/Shape/Rectangle.java
@@ -1,5 +1,21 @@
 package Shape;
 
-public class Rectangle {
-
+public class Rectangle implements Shape 
+{
+	private double length;
+	private double width;
+	public Rectangle(double length,double width)
+	{
+		this.length = length;
+		this.width = width;
+	}
+	public double perimeter()
+	{
+		return (length * 2 + width *2);
+	}
+	public double area()
+	{
+		return (length * width);
+	}
+	
 }
--- Classwork/src/Shape/Runner.java
+++ Classwork/src/Shape/Runner.java
@@ -1,5 +1,34 @@
 package Shape;
 
-public class Runner {
-
+public class Runner 
+{
+	public static void main(String[] args)
+	{
+		Rectangle rectangle1 = new Rectangle(3.14,1);
+		Circle circle1 = new Circle(1);
+		if(rectangle1.perimeter() > circle1.perimeter())
+		{
+			System.out.println("Rectangle has larger perimeter");
+		}
+		if(rectangle1.area() > circle1.area())
+		{
+			System.out.println("Rectangle has larger area");
+		}
+		if(circle1.perimeter() > rectangle1.perimeter())
+		{
+			System.out.println("Circle has larger perimeter");
+		}
+		if(circle1.area() > rectangle1.area())
+		{
+			System.out.println("Circle has larger area");
+		}
+		if(circle1.area() == rectangle1.area())
+		{
+			System.out.println("Same area");
+		}
+		if(circle1.perimeter() == rectangle1.perimeter())
+		{
+			System.out.println("Same perimeter");
+		}
+	}
 } 
