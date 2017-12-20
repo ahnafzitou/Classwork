2a5ce9a515ed4bbf2111d33a2367d68116c6d2cf
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2017-12-20 13:12:31 -0500

    pushy

--- csvstuff/src/csvstuff/Runner.java
+++ csvstuff/src/csvstuff/Runner.java
@@ -11,11 +11,11 @@ import java.io.File;
 public class Runner {
 	public static void main(String[] argsssss) throws IOException
 	{	
-		File CVS = new File("Infant_Mortality.csv");
+		File CVS = new File("data.csv");
 		CSVUtilities potato =  new CSVUtilities(CVS); 
 		
 		ArrayList<String> yes = new ArrayList<String>();
 		yes = (ArrayList<String>) potato.getColumnData(5);
-		potato.getColumnData(3);
+		potato.getColumnData(5);
 	}
 }
